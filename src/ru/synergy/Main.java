0cdc8083800b35af0ff9bcecedc6acb7ef32e040
YuraTal <jtalabaev@contractcity.ru>
2022-08-25 23:14:22 +0300

    Cats ArrayList Example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,12 +1,15 @@
 package ru.synergy;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
-        Cat [] cats = new Cat[3];
+        Cat [] cats = new Cat[4];
         cats[0] = new  Cat("Томас");
         cats[1] = new  Cat("Бегемот");
         cats[2] = new  Cat("Филип Маркович");
@@ -15,6 +18,14 @@ public class Main {
         //cats[1] = null;
 
         System.out.println(Arrays.toString(cats));
+
+        ArrayList<Cat> catsList = new ArrayList<>();
+        for(Cat cat : cats) {
+            catsList.add(cat);
+        }
+        catsList.add(new Cat("Бегемот2"));
+
+       System.out.println(catsList.toString());
 
     }
 }
